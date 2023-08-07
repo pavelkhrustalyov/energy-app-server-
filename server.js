0dec871670288f663b9b41442042fa46227b389e
Pavel <pavel_khrustalyov@mail.ru>
2023-08-07 16:12:34 +0300

    fix

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-require('dotenv').config({ path: "./config/.env" })
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
+require('dotenv').config({ path: './config/.env' });
 
 // import routes 
 const profileRoutes = require('./routes/profileRoutes');
@@ -12,20 +11,17 @@ const postRoutes = require('./routes/postRoutes');
 const authRoutes = require('./routes/authRoutes');
 const wallPostRoutes = require('./routes/wallPostsRoutes');
 
-app.use('/api', createProxyMiddleware({ target: 'http://localhost:5000' }));
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+});
 
 // import db
 const database = require('./database/db');
 
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -38,9 +34,13 @@ app.use('/energy', postRoutes);
 app.use('/energy', wallPostRoutes);
 app.use('/energy', authRoutes);
 
+// Serve React client on any other route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => {
     console.log('server is running on PORT ' + PORT);
-})
-
+});
